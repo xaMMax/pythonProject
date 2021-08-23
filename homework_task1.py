ffe807be849b1109d222573e6055c2624e2c2ce9
Maks Kaydash <36572876+xaMMax@users.noreply.github.com>
2021-08-23 11:26:55 +0300

    Delete homework_task1.py

--- homework_task1.py
+++
@@ -1,5 +0,0 @@
-yourStr = input('Please type your string here ') # input sample string
-if len(yourStr) >= 2:                            # use function len for compare and
-    expString = yourStr[:2] + yourStr[-2:]
-    print('your string is ' + yourStr, 'and expected result is ' + expString)
-else: print('empty string')
