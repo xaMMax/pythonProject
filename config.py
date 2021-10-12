97f1eeb62bb262d6e9205ec5a541f9d46d4dc8bf
Maks Kaydash <36572876+xaMMax@users.noreply.github.com>
2021-10-12 23:33:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # создаем класс для хранения конфигурационных данных
 class Config:
-    TOKEN = "2073168475:AAFN1NE7Xfb5YFFIXGiPlCBR-e-tGQTQRXI"  # токен полученный от ботфазера
+    TOKEN = "TOKEN"  # токен полученный от ботфазера
     list_start_commands = ["start"]  # перечень команд для запуска бота
     languages_dictionary = {"ru-RU": "Russian", "en-GB": "English",
                             "uk-UA": "Ukrainian", "pl-PL": "Poland",
